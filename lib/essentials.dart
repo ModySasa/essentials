6ae8a4d0cf051a22a4d672ea8bd86e935178a362
modysasa <msafwat16@yahoo.com>
2020-04-17 02:43:51 +0200

    Should be working

--- lib/essentials.dart
+++ lib/essentials.dart
@@ -313,7 +313,7 @@ Future downloadAPKFile(
   Dio dio = new Dio();
   await dio.download(currentAppLink, await UrlToFilename.file(), onReceiveProgress: (i, maxProgress) async {
     if (maxProgress != -1) {
-      while (i < maxProgress) {
+
         await Future.delayed(Duration(seconds: 1), () async {
           var androidPlatformChannelSpecifics = AndroidNotificationDetails(
             'download_apk',
@@ -333,7 +333,7 @@ Future downloadAPKFile(
               .show(122, 'Downloading', 'Donwloading progress ${(i * 100 / maxProgress).toStringAsFixed(0)} %', platformChannelSpecifics, payload: 'item x');
         });
       }
-    }
+
   }).whenComplete(() async {
     onClickInstallApk(await UrlToFilename.file(), myPackageName);
   });
